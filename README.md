50d052560f3f2fd769fdcf2ffc6e8c5714978b3e
Shivaprasad <shiv.ylp@gmail.com>
2023-05-01 22:45:53 +0530

    feat: add customisation options

--- README.md
+++ README.md
@@ -10,5 +10,22 @@ You are free to fork this and customise it to your needs.
 <script
   async
   src="https://cdn.jsdelivr.net/gh/docutalk/embed@latest/index.min.js"
-  data-bot-id="<your-bot-id>" />
+  data-bot-id="<your-bot-id>"
+  data-welcome="Hi, how may i assist you today?"
+  data-hint="Just ask me your question here"
+  data-sendbg="#151515"
+  data-userbg="#151515"
+  data-botbg="#151515"
+/>
 ```
+
+| Attribute  | Description                                                                                                                                |
+| ---------- | ------------------------------------------------------------------------------------------------------------------------------------------ |
+| `title`    | Title for the chat window                                                                                                                  |
+| `avatar`   | The avatar image to be shown in the floating button and the chat window. Empty value will either use config from DocuTalk or the 🤖 emoji. |
+| `noheader` | Setting this to `true` will disable the header.                                                                                            |
+| `welcome`  | A welcome message to be shown when a new chat begins                                                                                       |
+| `hint`     | Placeholder to show in the query input textbox                                                                                             |
+| `sendbg`   | Background color for the send button                                                                                                       |
+| `userbg`   | Background color for the user message bubble                                                                                               |
+| `botbg`    | Background color for the bot message bubble                                                                                                |
--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const scriptTag = document.currentScript;
 const botId = scriptTag.getAttribute("data-bot-id");
 
+const params = {
+  title: scriptTag.getAttribute("data-title"),
+  hint: scriptTag.getAttribute("data-hint"),
+  avatar: scriptTag.getAttribute("data-avatar"),
+  welcome: scriptTag.getAttribute("data-welcome"),
+  botbg: scriptTag.getAttribute("data-botbg"),
+  sendbg: scriptTag.getAttribute("data-sendbg"),
+  userbg: scriptTag.getAttribute("data-userbg"),
+  noheader: scriptTag.getAttribute("data-noheader"),
+};
+
+const embedParams = new URLSearchParams(
+  Object.fromEntries(
+    Object.entries(params).filter(
+      ([_, value]) => value !== undefined && value !== null
+    )
+  )
+);
+const embedUrl = new URL(`https://docutalk.co/embed/${botId}`);
+embedUrl.search = embedParams.toString();
+
 function setupPushToChatButton() {
   const btnChat = document.createElement("button");
   btnChat.setAttribute("id", "docutalk-push-to-talk");
-  btnChat.innerHTML =
-    '<img style="width: 30px;" src="https://docutalk.co/logo.png" alt="Button Image"/>';
+
+  if (params.avatar) {
+    btnChat.innerHTML = `<img style="width: 30px;" src="${params.avatar}" alt="Button Image"/>`;
+  } else {
+    btnChat.innerHTML = `<span style="font-size: 20px;">🤖</span>`;
+  }
 
   // add styles to position the button and make it circular
   btnChat.style.position = "fixed";
@@ -19,14 +44,14 @@ function setupPushToChatButton() {
   btnChat.style.boxShadow = "0px 0px 5px rgba(0, 0, 0, 0.3)";
   btnChat.style.padding = "0";
   btnChat.style.cursor = "pointer";
-
+  btnChat.style.fontSize = "20px";
   return btnChat;
 }
 
 function setupChatFrame(botId) {
   // create the iframe element
   const iframe = document.createElement("iframe");
-  iframe.src = "https://docutalk.co/embed/" + botId;
+  iframe.src = embedUrl.toString();
 
   // add styles to position the iframe and hide it initially
   iframe.style.position = "fixed";
@@ -69,19 +94,15 @@ document.body.appendChild(chatFrame);
 
 // set up the button image swapping logic
 const buttonImg = btnChat.querySelector("img");
-const buttonImgSrc = buttonImg.getAttribute("src");
-const buttonImgSrcX = "https://docutalk.co/logo.png";
-let isChatOpen = true;
+let isChatOpen = false;
 
 // add a click event listener to the button
 btnChat.addEventListener("click", function () {
   // toggle the visibility of the iframe
   if (isChatOpen) {
     chatFrame.style.display = "none";
-    buttonImg.setAttribute("src", buttonImgSrc);
   } else {
     chatFrame.style.display = "block";
-    buttonImg.setAttribute("src", buttonImgSrcX);
   }
   isChatOpen = !isChatOpen;
 });
